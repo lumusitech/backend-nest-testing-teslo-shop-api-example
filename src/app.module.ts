273535244824f23f473119689d4208b306b9b1bb
Luciano Figueroa <lumusitech@gmail.com>
2025-07-28 14:44:09 -0300

    chore: change db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { SeedModule } from './seed/seed.module';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
